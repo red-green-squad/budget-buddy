c3e0b188df1d0aa28bccc259371b25a337283bbf
Naresh <chakalinaresh1729@gmail.com>
2023-09-06 13:25:27 +0530

    BB-10: feat: add login page and google oauth

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Logo from '@/../public/icons/BudgetBuddy.svg';
+import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
@@ -8,6 +9,8 @@ import { useState } from 'react';
 
 export const Header = () => {
   const router = useRouter();
+  const { data } = useSession();
+
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const handleSignIn = () => {
@@ -20,6 +23,8 @@ export const Header = () => {
     router.push('/signup');
   };
 
+  console.log(data);
+
   return (
     <nav className="bg-white sticky w-full z-20 top-0 left-0 border-b border-gray-200 ">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
@@ -29,20 +34,38 @@ export const Header = () => {
             BudgetBuddy
           </span>
         </Link>
-        <div className="md:flex hidden md:order-2 md:gap-8">
-          <button
-            onClick={handleSignUp}
-            className="text-white bg-gradient-to-r from-sky-500 to-indigo-500 hover:bg-gradient-to-r hover:from-sky-600 hover:to-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 "
-          >
-            SignUp
-          </button>
-          <button
-            onClick={handleSignIn}
-            className="text-white bg-gradient-to-r from-sky-500 to-indigo-500 hover:bg-gradient-to-r hover:from-sky-600 hover:to-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 "
-          >
-            SingIn
-          </button>
-        </div>
+        {!data && (
+          <div className="md:flex hidden md:order-2 md:gap-8">
+            <button
+              onClick={handleSignUp}
+              className="text-white bg-gradient-to-r from-sky-500 to-indigo-500 hover:bg-gradient-to-r hover:from-sky-600 hover:to-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 "
+            >
+              SignUp
+            </button>
+            <button
+              onClick={handleSignIn}
+              className="text-white bg-gradient-to-r from-sky-500 to-indigo-500 hover:bg-gradient-to-r hover:from-sky-600 hover:to-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 "
+            >
+              SingIn
+            </button>
+          </div>
+        )}
+        {data && (
+          <div className="md:flex hidden md:order-2 md:gap-8">
+            <button
+              onClick={handleSignUp}
+              className="text-white bg-gradient-to-r from-sky-500 to-indigo-500 hover:bg-gradient-to-r hover:from-sky-600 hover:to-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 "
+            >
+              Expenses
+            </button>
+            <button
+              onClick={handleSignIn}
+              className="text-white bg-gradient-to-r from-sky-500 to-indigo-500 hover:bg-gradient-to-r hover:from-sky-600 hover:to-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 "
+            >
+              Dashboard
+            </button>
+          </div>
+        )}
         <button
           data-collapse-toggle="navbar-sticky"
           type="button"
@@ -69,34 +92,60 @@ export const Header = () => {
           </svg>
         </button>
       </div>
-      <div
-        id="dropdown"
-        className={`md:hidden z-10 ${
-          isDropdownOpen ? 'block' : 'hidden'
-        } bg-white divide-y divide-gray-100 rounded-lg shadow w-full`}
-      >
-        <ul
-          className="py-2 text-sm text-gray-800 w-full text-center"
-          aria-labelledby="dropdownDefaultButton"
+      {
+        <div
+          id="dropdown"
+          className={`md:hidden z-10 ${
+            isDropdownOpen ? 'block' : 'hidden'
+          } bg-white divide-y divide-gray-100 rounded-lg shadow w-full`}
         >
-          <li>
-            <button
-              onClick={handleSignUp}
-              className="block w-full px-4 py-2 hover:bg-purple-300 "
+          {data ? (
+            <ul
+              className="py-2 text-sm text-gray-800 w-full text-center"
+              aria-labelledby="dropdownDefaultButton"
             >
-              SignUp
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={handleSignIn}
-              className="block w-full px-4 py-2 hover:bg-purple-300 "
+              <li>
+                <button
+                  onClick={handleSignIn}
+                  className="block w-full px-4 py-2 hover:bg-purple-300 "
+                >
+                  Dashboard
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={handleSignUp}
+                  className="block w-full px-4 py-2 hover:bg-purple-300 "
+                >
+                  Expenses
+                </button>
+              </li>
+            </ul>
+          ) : (
+            <ul
+              className="py-2 text-sm text-gray-800 w-full text-center"
+              aria-labelledby="dropdownDefaultButton"
             >
-              SingIn
-            </button>
-          </li>
-        </ul>
-      </div>
+              <li>
+                <button
+                  onClick={handleSignUp}
+                  className="block w-full px-4 py-2 hover:bg-purple-300 "
+                >
+                  SignUp
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={handleSignIn}
+                  className="block w-full px-4 py-2 hover:bg-purple-300 "
+                >
+                  SingIn
+                </button>
+              </li>
+            </ul>
+          )}
+        </div>
+      }
     </nav>
   );
 };
---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
+
+const ProtectedRoutes = ['/expenses'];
+const AuthRoutes = ['/signin', '/signup'];
+
+export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const token = await getToken({ req: request });
+
+  if (ProtectedRoutes.some((path) => pathname.startsWith(path))) {
+    if (!token) {
+      const redirectUrl = new URL('/signin', request.nextUrl);
+      return NextResponse.redirect(redirectUrl);
+    }
+  } else if (AuthRoutes.some((route) => pathname.startsWith(route))) {
+    if (token) {
+      const redirectUrl = new URL('/', request.nextUrl);
+      return NextResponse.redirect(redirectUrl);
+    }
+  }
+  return NextResponse.next();
+}
