b5aae1627bc6f1a9ad887f1af2b0f6a67ff0fb7e
Naresh <chakalinaresh1729@gmail.com>
2023-09-28 15:32:20 +0530

    BB-00: fix: update user model imports

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,5 +1,5 @@
 import { connectToDB } from '@/db/mongodb';
-import { UserModel } from '@/models/user';
+import { UserModel } from '@/models/User';
 import bcryptjs from 'bcryptjs';
 import { NextRequest, NextResponse } from 'next/server';
 
