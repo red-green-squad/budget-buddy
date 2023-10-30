d55f4ece32b0bfce8a37bb53f0965808e9326db3
Naresh <chakalinaresh1729@gmail.com>
2023-10-30 10:08:01 +0530

    BB-18: minor: add summary api

--- src/components/dashboard/RecentExpenses.tsx
+++ src/components/dashboard/RecentExpenses.tsx
@@ -1,28 +1,51 @@
-import { ExpenseItem as IExpenseItem } from '@/types/expenses';
+'use client';
+import { ExpenseRequestBody } from '@/app/api/expenses/route';
+import { useAsync } from '@/hooks/useAsync';
+import { ExpenseListPage } from '@/types/expenses';
+import axios, { AxiosResponse } from 'axios';
+import { useEffect } from 'react';
+import { Loader } from '../common/Loader';
+import { EmptyList } from '../common/table/EmptyList';
 import { ExpenseItem } from '../expenses/ExpenseItem';
 
-const expenseItems: IExpenseItem[] = [
-  {
-    id: '1',
-    name: 'name',
-    amount: 400,
-    category: 'Charity and Donations',
-    files: [],
-    createdAt: new Date(),
-    date: new Date(),
-    description: 'a',
-    updatedAt: new Date(),
-  },
-];
 export const RecentExpenses = () => {
+  const [{ isLoading, error, data }, getRecentExpenses] = useAsync<
+    AxiosResponse<ExpenseListPage>,
+    ExpenseRequestBody
+  >({
+    fn: ({ pagination, sort, filters }) => {
+      return axios.get('/api/expenses', {
+        params: {
+          pagination: JSON.stringify(pagination),
+          sort: JSON.stringify(sort),
+          filters: JSON.stringify(filters),
+        },
+      });
+    },
+  });
+
+  useEffect(() => {
+    getRecentExpenses({
+      filters: [],
+      sort: { field: 'date', value: 'desc' },
+      pagination: { page: 1, pageSize: 10 },
+    });
+  }, []);
+
+  const items = data?.data.items || [];
+
   return (
     <div className="flex flex-col gap-2">
       <p className="text-2xl text-center">Recent transactions</p>
-      <div className="flex flex-col gap-2">
-        {expenseItems.map((item) => {
-          return <ExpenseItem item={item} key={item.id} />;
-        })}
-      </div>
+      {!isLoading && !error && items.length === 0 && <EmptyList />}
+      {isLoading && !error && <Loader />}
+      {!isLoading && !error && items.length > 0 && (
+        <div className="flex flex-col gap-2">
+          {items.map((item) => {
+            return <ExpenseItem item={item} key={item.id} />;
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/dashboard/Summary.tsx
+++ src/components/dashboard/Summary.tsx
@@ -1,10 +1,55 @@
 'use client';
-import { useState } from 'react';
+import { ExpenseSummaryRequestBody } from '@/app/api/summary/route';
+import { useAsync } from '@/hooks/useAsync';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { ChartType, Toolbar } from './Toolbar';
 
 export const Summary = () => {
   const [selectedYear, setSelectedYear] = useState<number>();
   const [chartType, setChartType] = useState<ChartType>('categories');
+
+  const [yearSuggestions, getYearSuggestions] = useAsync<
+    { label: string; value: number }[],
+    any
+  >({
+    fn: async () => {
+      const response = await axios.get('api/summary/year-suggestions');
+
+      const yearOptions =
+        response.data?.years.map((year: number) => ({
+          label: String(year),
+          value: year,
+        })) ?? [];
+      setSelectedYear(yearOptions[0].value);
+      return yearOptions;
+    },
+  });
+
+  const [expensesSummary, getExpensesSummary] = useAsync<
+    any,
+    ExpenseSummaryRequestBody
+  >({
+    fn: async (data) => {
+      const response = await axios.post('api/summary', data);
+      return response.data;
+    },
+  });
+
+  useEffect(() => {
+    getYearSuggestions({});
+  }, []);
+
+  useEffect(() => {
+    if (selectedYear) {
+      getExpensesSummary({
+        year: selectedYear,
+        groupBy: chartType === 'categories' ? 'category' : 'month',
+      });
+    }
+  }, [chartType, selectedYear]);
+
+  console.log(expensesSummary.data);
 
   return (
     <div className="flex flex-col gap-2">
@@ -12,12 +57,7 @@ export const Summary = () => {
       <Toolbar
         selectedYear={selectedYear}
         chartType={chartType}
-        yearOptions={[
-          { label: '2021', value: 2021 },
-          { label: '2022', value: 2022 },
-          { label: '2023', value: 2023 },
-          { label: '2024', value: 2024 },
-        ]}
+        yearOptions={yearSuggestions.data ?? []}
         onYearChange={setSelectedYear}
         onChartTypeChange={setChartType}
       />
